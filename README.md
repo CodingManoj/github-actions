f763496ace1f1b2ca7ce3ca7f33739801f344d3d
CodingManoj <CodingManoj@gmail.com>
2024-03-29 18:29:20 -0400

    TF Backend Infra Auto Push

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # github-actions
+
+    * We don't need to install any plugins on the runners
+    * You can define n number of actions on the workflows/
+    
---
+++ src/app.py
@@ -0,0 +1,8 @@
+# performs addition
+def add(a, b):
+    return a + b
+
+# performs unit testing
+def test_add():
+    assert add(1, 2) == 3
+    assert add(1, -1) == 0
